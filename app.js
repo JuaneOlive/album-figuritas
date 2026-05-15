82f5c90587fb7ba09e990668660fb2a00978ebf3
Juan Elías Olive Juarez <401723@sistemas.frc.utn.edu.ar>
2026-05-14 22:27:03 -0300

    Fix: ordenar figuritas según estructura del mundial
    - FWC (0-19)
    - CC (1-14)
    - Luego por grupos: A (MEX, RSA, KOR, CZE), B, C, ..., L
    - Dentro de cada país: número 1-20
    - Usa CASE WHEN para ordenamiento personalizado por código
    - Ordena secundariamente por número

--- app.js
+++ app.js
@@ -58,9 +58,68 @@ function buildStickerFilters(query) {
 async function findStickers(query) {
   const stickerFilters = buildStickerFilters(query);
 
+  const { sequelize } = require('sequelize');
+  const { literal } = require('sequelize');
+
   const stickers = await Sticker.findAll({
     where: stickerFilters,
-    include: [{ model: StickerType, as: 'tipo' }]
+    include: [{ model: StickerType, as: 'tipo' }],
+    order: [
+      [literal(`CASE
+        WHEN codigo = 'FWC' THEN 0
+        WHEN codigo = 'CC' THEN 1
+        WHEN codigo = 'MEX' THEN 2
+        WHEN codigo = 'RSA' THEN 3
+        WHEN codigo = 'KOR' THEN 4
+        WHEN codigo = 'CZE' THEN 5
+        WHEN codigo = 'CAN' THEN 6
+        WHEN codigo = 'BIH' THEN 7
+        WHEN codigo = 'QAT' THEN 8
+        WHEN codigo = 'SUI' THEN 9
+        WHEN codigo = 'BRA' THEN 10
+        WHEN codigo = 'MAR' THEN 11
+        WHEN codigo = 'HAI' THEN 12
+        WHEN codigo = 'SCO' THEN 13
+        WHEN codigo = 'USA' THEN 14
+        WHEN codigo = 'PAR' THEN 15
+        WHEN codigo = 'AUS' THEN 16
+        WHEN codigo = 'TUR' THEN 17
+        WHEN codigo = 'GER' THEN 18
+        WHEN codigo = 'CUW' THEN 19
+        WHEN codigo = 'CIV' THEN 20
+        WHEN codigo = 'ECU' THEN 21
+        WHEN codigo = 'NED' THEN 22
+        WHEN codigo = 'JPN' THEN 23
+        WHEN codigo = 'SWE' THEN 24
+        WHEN codigo = 'TUN' THEN 25
+        WHEN codigo = 'BEL' THEN 26
+        WHEN codigo = 'EGY' THEN 27
+        WHEN codigo = 'IRN' THEN 28
+        WHEN codigo = 'NZL' THEN 29
+        WHEN codigo = 'ESP' THEN 30
+        WHEN codigo = 'CPV' THEN 31
+        WHEN codigo = 'KSA' THEN 32
+        WHEN codigo = 'URU' THEN 33
+        WHEN codigo = 'FRA' THEN 34
+        WHEN codigo = 'SEN' THEN 35
+        WHEN codigo = 'IRQ' THEN 36
+        WHEN codigo = 'NOR' THEN 37
+        WHEN codigo = 'ARG' THEN 38
+        WHEN codigo = 'ALG' THEN 39
+        WHEN codigo = 'AUT' THEN 40
+        WHEN codigo = 'JOR' THEN 41
+        WHEN codigo = 'POR' THEN 42
+        WHEN codigo = 'COD' THEN 43
+        WHEN codigo = 'UZB' THEN 44
+        WHEN codigo = 'COL' THEN 45
+        WHEN codigo = 'ENG' THEN 46
+        WHEN codigo = 'CRO' THEN 47
+        WHEN codigo = 'GHA' THEN 48
+        WHEN codigo = 'PAN' THEN 49
+        ELSE 50
+      END`), 'ASC'],
+      ['numero', 'ASC']
+    ]
   });
 
   return stickers;
