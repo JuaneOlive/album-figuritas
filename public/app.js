664a82f5b8eef6f0cabbb208bf1a14771a2ce144
Juan Elías Olive Juarez <401723@sistemas.frc.utn.edu.ar>
2026-05-14 23:29:34 -0300

    feat: reset sticker autocomplete input after add/remove
    - Added resetStickerInput() function to clear input text and close dropdown
    - Function dispatchEvent('input') to trigger autocomplete filter update
    - Call resetStickerInput() after successful PATCH in both Agregar/Quitar buttons
    - Input is now properly cleared after each action

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 import { populateCountryCodeButtons } from "./countryButtons.js";
-import { initStickerAutocomplete } from "./select.js";
+import { initStickerAutocomplete, resetStickerInput } from "./select.js";
 import {
     clearStickersTable,
     initTableButtons,
@@ -103,6 +103,7 @@ function initStickerForm() {
 
         if (updatedSticker) {
             updateRenderedStickerQuantity(updatedSticker);
+            resetStickerInput();
         }
     });
 
@@ -112,6 +113,7 @@ function initStickerForm() {
 
         if (updatedSticker) {
             updateRenderedStickerQuantity(updatedSticker);
+            resetStickerInput();
         }
     });
 }
